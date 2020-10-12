7dbbc3f3a733f5dc320bed3a0c8091b198d4765e
Nicole Stock <nicoleelizabeth888@gmail.com>
2020-10-11 22:57:19 -0400

    Updating contact page and adding animation

--- js/contact.js
+++ js/contact.js
@@ -2,11 +2,36 @@ const submitButton = document.getElementById('submit');
 const firstNameInput = document.getElementById('first-name');
 const lastNameInput = document.getElementById('last-name');
 const emailInput = document.getElementById('email');
+const afterSubmitMessage = document.getElementById('after-submit-message');
+const arrow = document.getElementById('arrow');
+
+function delay(millis) {
+    return new Promise(resolve => setTimeout(resolve, millis));
+}
+
+function bounceArrow() {
+    arrow.classList.add('bounce-right');
+    window.setInterval(bounceRight, 500);
+}
+
+function bounceRight() {
+    arrow.classList.toggle('bounce-right');
+}
+
+function respondToSubmit() {
+    submitButton.classList.remove('clicked');
+    submitButton.innerHTML = "SUBMIT";
+    afterSubmitMessage.classList.add('submitted');
+    arrow.classList.add('submitted');
+    bounceArrow();
+}
 
 submitButton.addEventListener('click', () => {
+    submitButton.classList.add('clicked');
     firstNameInput.value = "";
     lastNameInput.value = "";
     emailInput.value = "";
-    //TODO display animation ??
-    // display "We're so excited to have you in WIT" or something of the like
+
+    submitButton.innerHTML = "WELCOME!";
+    delay(500).then( () => respondToSubmit());
 });
